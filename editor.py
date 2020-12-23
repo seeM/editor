aa9d7bcec3b6c2bc2130f329b5c67f99af8da36e
seem <mwlorgat@gmail.com>
2020-12-23 16:54:39 +0200

    Read and display buffer

--- editor.py
+++ editor.py
@@ -1,9 +1,21 @@
+import argparse
 import curses
 import sys
 
 
 def main(stdscr):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    args = parser.parse_args()
+
+    with open(args.filename) as f:
+        buffer = f.readlines()
+
     while True:
+        stdscr.erase()
+        for line, string in enumerate(buffer):
+            stdscr.addstr(line, 0, string)
+
         k = stdscr.getkey()
         if k == "q":
             sys.exit(0)
